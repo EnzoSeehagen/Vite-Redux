6a890725c76e879ca8ef9b29e4de610866a7d445
EnzoSeehagen <enzovonseehagen@gmail.com>
2022-12-07 02:12:55 -0300

    Dispachando a action

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,16 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import { connect } from 'react-redux'
 
-function App({contagem}) {
+
+function App({contagem, increment}) {
+  function handleOnClick(){
+    increment();
+  }
   return (
     <div className="App">
       <h1>Redux</h1>
       <div className="card">
-        <button>
+        <button onClick={handleOnClick}>
           a contagem é {contagem}
         </button>
       </div>
@@ -20,5 +24,12 @@ const mapStateToProps = (state) => {
   return {
     contagem: state.counter.value
   }
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    increment: () => dispatch({type: "counter/increment"}),
+  }
 }
-export default connect(mapStateToProps)(App);
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
